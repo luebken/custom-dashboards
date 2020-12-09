002243c8f9cd7f46058bcf18f70745656a902011
Matthias Luebken <matthias.luebken@gmail.com>
2020-12-09 16:12:33 +0100

    fetch

--- custom-dashboards.js
+++ custom-dashboards.js
@@ -1,46 +1,44 @@
 const fetch = require('node-fetch');
 
-const api_token = process.env.INSTANA_API_TOKEN
-if (process.argv.length != 3 || !api_token) {
-    console.error("Usage: INSTANA_API_TOKEN=<INSTANA_API_TOKEN> node custom-dashboards.js <INSTANA_API_URL>")
-    return
+const apiToken = process.env.INSTANA_API_TOKEN;
+if (process.argv.length != 3 || !apiToken) {
+  console.error('Usage: INSTANA_API_TOKEN=<INSTANA_API_TOKEN> node custom-dashboards.js <INSTANA_API_URL>');
+  return;
 }
-const api_url = process.argv[2]
+const url = process.argv[2];
 
-const body = {
-    "id": "vckk-dYcRU6z04QP37F8cQA",
-    "title": "Test matthias",
-    "accessRules": [
-        {
-            "accessType": "READ_WRITE",
-            "relationType": "USER",
-            "relatedId": "5ee8a3e8cd70020001ecb007"
-        }
-    ],
-    "widgets": [
-        {
-            "id": "uzmv5wyiJOAlrP8vA",
-            "title": "test",
-            "width": 1,
-            "height": 1,
-            "x": 0,
-            "y": 0,
-            "type": "markdown",
-            "config": "test"
-        }
-    ],
-    "writable": true
-}
-
-
-console.log("Post")
-fetch(api_url + 'custom-dashboard', {
-    method: 'post',
-    body: JSON.stringify(body),
+(async () => {
+  const response = await fetch(`${url}/api/custom-dashboard`, {
+    method: 'POST',
     headers: {
-        'authorization': 'apiToken ' + api_token,
-        'content-type': 'application/json'
+      authorization: `apiToken ${apiToken}`,
+      'content-type': 'application/json'
     },
-})
-    .then(res => res.json())
-    .then(json => console.log(json));
+    body: JSON.stringify({
+      title: 'Test Matthias Generated Dashboard',
+      accessRules: [
+        {
+          accessType: 'READ_WRITE',
+          relationType: 'USER',
+          relatedId: '5ee8a3e8cd70020001ecb007'
+        }
+      ],
+      widgets: [
+        {
+          id: Math.random().toString(36).substring(7),
+          title: 'Test Widget Title',
+          width: 1,
+          height: 1,
+          x: 0,
+          y: 0,
+          type: 'markdown',
+          config: 'Test markdown: ' + new Date()
+        }
+      ]
+    })
+  });
+
+  console.log('Status code:', response.status);
+  const responseBody = await response.text();
+  console.log('Response body:', responseBody);
+})().catch(e => console.error('Error', e));
