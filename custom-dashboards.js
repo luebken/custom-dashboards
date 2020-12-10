1b8f3feb664b91e58ead0d7a6e8564d410e6d179
Matthias Luebken <matthias.luebken@gmail.com>
2020-12-10 09:18:41 +0100

    url as a parameter

--- custom-dashboards.js
+++ custom-dashboards.js
@@ -6,19 +6,18 @@ const fs = require('fs');
 const spec_dir = "./specs/";
 
 const apiToken = process.env.INSTANA_API_TOKEN;
-if (process.argv.length != 3 || !apiToken) {
-    console.error('Usage: INSTANA_API_TOKEN=<INSTANA_API_TOKEN> node custom-dashboards.js <INSTANA_API_URL>');
+if (process.argv.length != 4 || !apiToken) {
+    console.error('Usage: INSTANA_API_TOKEN=<INSTANA_API_TOKEN> node custom-dashboards.js <INSTANA_URL> <KUBERNETES_NAMESPACE_NAME>');
     return;
 }
 const url = process.argv[2];
-
+const kubernetes_namespace_name = process.argv[3];
 
 fs.readdir(spec_dir, (err, files) => {
     files.forEach(file => {
         let rawdata = fs.readFileSync(spec_dir + file);
 
-        var kubernetes_namespace_name = "robot-shop"
-        let json = replaceAndParseJson(rawdata.toString(), kubernetes_namespace_name)
+        let json = replaceAndParseJson(rawdata.toString(), kubernetes_namespace_name, url)
         console.log(json)
         console.log("Creating dashboard defined in " + file);
         (async () => {
@@ -39,12 +38,13 @@ fs.readdir(spec_dir, (err, files) => {
 });
 
 // dummy jsonnet parser
-// removes replaces kubernetes_namespace_name
+// removes replaces KUBERNETES_NAMESPACE_NAME
 // TODO read in variables from the json file
-function replaceAndParseJson(jsonTemplate, kubernetes_namespace_name) {
+function replaceAndParseJson(jsonTemplate, kubernetes_namespace_name, url) {
     //var result = jsonTemplate.toString().replace(/local .*\n/g, '');
     //result = jsonTemplate.toString().replace(/\/\/.*\n/g, '');
-    var result = jsonTemplate.toString().replace(/\$kubernetes_namespace_name/g, kubernetes_namespace_name);
+    var result = jsonTemplate.toString().replace(/\$KUBERNETES_NAMESPACE_NAME/g, kubernetes_namespace_name);
+    result = result.replace(/\$INSTANA_URL/g, url);
     return JSON.parse(result)
 }
 
